ed7c76a0428adf0fe916426c86ab69377c2d03b9
jaesung <cjs6027@naver.com>
2020-03-02 02:00:49 +0900

    스킬체크 레벨3 2트 1번

---
+++ Programers/스킬체크 레벨3 2트 1번.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, long long k) {
+	vector<int> answer;
+	vector<int> tmp;
+	long long counts = 1;
+	for (int i = 1; i <= n; i++) {
+		counts *= i;
+		tmp.push_back(i);
+	}
+	// /로 나눠서 위치를 직접 찾아간다.
+	while (n > 0) {
+		long long cut = counts / n;
+		int now = (k - 1) / cut;
+		answer.push_back(tmp.at(now));
+		tmp.erase(tmp.begin() + now);
+		k = k % cut;
+		if (k == 0)
+			k = cut;
+		counts = cut;
+		n--;
+	}
+	return answer;
+}
+
+int main() {
+	solution(3, 5);
+}
