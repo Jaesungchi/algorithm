f155bfb5195318b844130d10c20c07520e5bb8a9
jaesung <cjs6027@naver.com>
2020-03-27 22:00:39 +0900

    CodeForces Round 629 (Div.3)

---
+++ Codeforces/629_A.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void run() {
+	int a, b,output = 0,tmp;
+	cin >> a >> b;
+	if (a < b) {
+		output = b - a;
+	}
+	else if (a%b != 0) {
+		tmp = b * (a / b) + b;
+		output = tmp - a;
+	}
+	cout << output << "\n";
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--)
+		run();
+}
---
+++ Codeforces/629_B.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// b의 최대는 n(n-1) / 2 이다.
+
+void run() {
+	int a, b, i,tmp;
+	cin >> a >> b;
+	for (i = a; i > 1 ; i--) {
+		tmp = (i*(i - 1)) / 2;
+		if (tmp < b)
+			break;
+	}
+	//i ~ i-1 의 사이에 있다. ex i == 5 면 baaab ~ bbaaa 7 ~ 10 사이.
+	string output = "b";
+	for (int j = 0; j < i; j++)
+		output += "a";
+	i++;
+	output[(i*(i - 1) / 2) - b+1] = 'b';
+	for (i = output.length(); i < a; i++)
+		cout << "a";
+	cout << output << "\n";
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--)
+		run();
+}
